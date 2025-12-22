4a271d2d8c95893dba697427ea4dc4c9c4b93294
lyhoangducuy <lyhoangducuy@gmail.com>
2025-12-22 15:26:19 +0700

    first commit

---
+++ src/main/java/com/example/quanlyhusc/QuanlyhuscApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlyhusc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyhuscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyhuscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quanlyhusc/QuanlyhuscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlyhusc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyhuscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
